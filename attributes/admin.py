8e7bf9f6785144c82b1dffeee19cfcd0cf830943
Colin Powell <colin@onecardinal.com>
2011-05-05 01:03:38 -0400

    Adding a missing funciton.

--- attributes/admin.py
+++ attributes/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from attributes.models import AttributeOption
+from attributes.utils import validate_attribute_value
+
+def clean_attribute_value(cleaned_data, obj):
+    value = cleaned_data['value']
+    attribute = cleaned_data['option']
+    success, valid_value = validate_attribute_value(attribute, value, obj)
+    if not success:
+        raise ValidationError(attribute.error_message)
+    
+    return valid_value
 
 admin.site.register(AttributeOption)
